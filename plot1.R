9194bc2da71103f49462b317eddabac64d1897a4
ahmed-elbaz <37707382+ahmed-elbaz@users.noreply.github.com>
2018-05-01 13:26:47 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ subData <- subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007" )
 
 png(filename = "plot1.png", width = 480, height = 480 )
 
-# plotting the histogram
+# plotting
 
 hist(as.numeric(as.character(subData$Global_active_power)), col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 
